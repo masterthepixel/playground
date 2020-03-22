87fb2ff09835cd4e8400dc61ade4bc46a82c95e6
masterthepixel <45719243+masterthepixel@users.noreply.github.com>
2020-03-22 18:07:04 -0400

    InterFont Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
 module.exports = {
   theme: {
-    extend: {},
+    interFontFeatures: {
+      default: ['calt', 'liga', 'kern'],
+      numeric: ['tnum', 'salt', 'ss02']
+    },
+    extend: {
+      fontSize: {
+        'xs': '11px',
+        's': '13px',
+        'm': '17px',
+        'l': '28px',
+        'xl': '36px',
+        '2xl': '48px',
+        '3xl': '64px',
+        base: '16px',
+      },
+      lineHeight: {
+        'lg': '1.6',
+        'md': '1.33',
+        'sm': '1.25',
+        'xs': '1',
+      },
+    }
   },
-  variants: {},
-  plugins: [],
-}
+  plugins: [
+    require('tailwindcss-font-inter')({ // it's plugin's default settings
+      a: -0.0223,
+      b: 0.185,
+      c: -0.1745,
+      baseFontSize: 16,
+      baseLineHeight: 1.5,
+      importFontFace: false,
+      disableUnusedFeatures: false
+    })
+  ]
+};
